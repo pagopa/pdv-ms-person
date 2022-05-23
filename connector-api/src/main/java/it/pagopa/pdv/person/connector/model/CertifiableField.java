b177fdb59d9b643d6f7bb329c8050e05be121062
Rodolfo Viti <rodolfo.viti@nttdata.com>
2022-05-23 15:48:14 +0200

    add certification fields check

--- connector-api/src/main/java/it/pagopa/pdv/person/connector/model/CertifiableField.java
+++ connector-api/src/main/java/it/pagopa/pdv/person/connector/model/CertifiableField.java
@@ -2,6 +2,8 @@ package it.pagopa.pdv.person.connector.model;
 
 public interface CertifiableField<T> {
 
+    String NOT_CERTIFIED_VALUE = "NONE";
+
     String getCertification();
 
     void setCertification(String certification);
