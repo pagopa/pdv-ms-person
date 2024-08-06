abce7a107f87d4277d4c06045ac993c12848cd6a
Giuseppe Gangemi <120578920+giuseppe-gangemi@users.noreply.github.com>
2024-08-06 09:20:42 +0200

    chore: refactor suggested change
    Co-authored-by: Benito Visone <benito.visone@pagopa.it>

--- connector-api/src/main/java/it/pagopa/pdv/person/connector/model/PersonDetailsOperations.java
+++ connector-api/src/main/java/it/pagopa/pdv/person/connector/model/PersonDetailsOperations.java
@@ -24,6 +24,7 @@ public interface PersonDetailsOperations {
         CertifiableField<String> getEmail();
 
         CertifiableField<String> getMobilePhone();
+        
         CertifiableField<String> getTelephone();
 
     }
